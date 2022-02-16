2a8b955f7b0e6b48b754ab74c240eb19d6adfb1a
justinagain <jmdv@buffalo.edu>
2022-02-16 15:44:59 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.  Edits made 
-// by JD.
+// by JD.  More changes to come!
 //
 // Contributor: Julien Vehent jvehent@mozilla.com [:ulfr]
 package main
